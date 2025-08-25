624bc953cd3843df835e3cf8b04d3b45bfaf115b
Abhiany <negia394@gmail.com>
2025-08-25 10:14:04 +0530

    Created appium automation framework with some practice tests

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,114 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Pause;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities; // not used, but keeps IDE suggestions happy
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BaseTest {
+    protected AndroidDriver driver;
+    protected WebDriverWait wait;
+
+    PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // --- IMPORTANT: start the Appium server separately: `appium` ---
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setPlatformName("Android")
+                .setAutomationName("UiAutomator2") // which driver we are using to automate
+                .setDeviceName("emulator-5554")// or your real-device name from `adb devices`
+                // ---- OPTION 1: provide an APK path (ensure it targets SDK >= 24)
+                //.setApp("C:\\Users\\<you>\\Downloads\\YourModernSampleApp.apk")
+                // ---- OPTION 2: launch an already-installed app using package/activity:
+                .setAppPackage("io.appium.android.apis") // the name of the already installed app
+                .setAppActivity(".ApiDemos")        // adjust to your app’s launch activity or a particular screen of the app
+                .setAutoGrantPermissions(true) // automatically grant all permission
+                .setNewCommandTimeout(Duration.ofSeconds(3600)); // how long to keep connection active if tests does not send command
+
+        driver = new AndroidDriver(
+                URI.create("http://127.0.0.1:4723/").toURL(),
+                options
+        );
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // small implicit
+    }
+
+    public WebElement scrollToTheElementByText(String text){
+        return driver.findElement(AppiumBy.androidUIAutomator(
+                "new UiScrollable(new UiSelector().scrollable(true))" +
+                        ".scrollIntoView(new UiSelector().text(\""+text+"\"))"
+        ));
+    }
+
+    public void performLongPress(WebElement element) {
+        /*
+          PointerInput = represents an input source (like a finger, mouse, pen).
+          Here: we create a touch pointer called "finger".
+          All subsequent actions will use this finger.
+         */
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        /*
+            A Sequence = a set of actions performed in order by this pointer.
+            We create a sequence called longPress, tied to "finger".
+            The 1 is just the sequence ID.
+         */
+        Sequence longPress = new Sequence(finger, 1);
+
+        // Moving the finger to x,y coordinate of the web element, here the finger is just hovering over the element
+        longPress.addAction(finger.createPointerMove(Duration.ofMillis(0),
+                PointerInput.Origin.viewport(), element.getRect().getX(), element.getRect().getY()));
+        // Simulate touch down of the finger on the desired coordinate
+        longPress.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        longPress.addAction(new Pause(finger, Duration.ofSeconds(2))); // hold for 2 sec
+        // lift finger up
+        longPress.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        // Perform the sequence of long press
+        driver.perform(Arrays.asList(longPress));
+
+    }
+
+    public void swipeLeftImageGesture(WebElement element) {
+        int startX = element.getRect().getX() + (int)(element.getSize().width * 0.9); // near right edge
+        int endX   = element.getRect().getX() + (int)(element.getSize().width * 0.6); // near left edge
+        int y      = element.getRect().getY() + (element.getSize().height / 2);       // middle of image
+
+        Sequence swipeLeft = new Sequence(finger, 1);
+        swipeLeft.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(),
+                startX, y));
+        swipeLeft.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
+        swipeLeft.addAction(finger.createPointerMove(Duration.ofMillis(100), PointerInput.Origin.viewport(), endX, 2));
+        swipeLeft.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        driver.perform(Collections.singletonList(swipeLeft));
+    }
+
+    public void dragNDropGesture(WebElement src, WebElement dest){
+            
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Driver quited");
+        }
+    }
+}
---
+++ src/test/java/tests/CommonMobileActionsTest.java
@@ -0,0 +1,78 @@
+package tests;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class CommonMobileActionsTest extends BaseTest{
+
+    @Test
+    public void startApp() throws InterruptedException {
+//        driver.findElement(AppiumBy.accessibilityId("Accessibility")).click();
+//        driver.findElement(AppiumBy.id("android:id/text1")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Accessibility']")).click();
+    }
+
+    @Test
+    public void enterText() {
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+        driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.RelativeLayout\").instance(1)")).click();
+        driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("Hello");
+        driver.findElement(AppiumBy.id("android:id/button1")).click();
+    }
+
+    @Test
+    public void printTextOfAllElements() {
+        List<WebElement> elements = driver.findElements(AppiumBy.className("android.widget.TextView"));
+        for(WebElement e : elements){
+            System.out.println(e.getText());
+        }
+    }
+
+    @Test
+    public void verifyText() {
+        List<WebElement> elements = driver.findElements(AppiumBy.className("android.widget.TextView"));
+        String actualBtnText = elements.get(1).getText();
+        Assert.assertEquals(actualBtnText, "Accessibility");
+        System.out.println("First button text verified!");
+    }
+
+    @Test
+    public void scrollToElement() {
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Views']")).click();
+        scrollToTheElementByText("WebView").click();
+    }
+
+    @Test
+    public void longPress() {
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Views']")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Expandable Lists']")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='1. Custom Adapter']")).click();
+        WebElement e = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"People Names\")"));
+        performLongPress(e);
+        driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Sample action\")")).click();
+    }
+
+    @Test
+    public void swipeLeft() {
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Views']")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Gallery']")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='1. Photos']")).click();
+        WebElement e = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().className(\"android.widget.ImageView\").instance(0)"));
+        swipeLeftImageGesture(e);
+    }
+
+    @Test
+    public void dragNDrop() {
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Views']")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc='Drag and Drop']")).click();
+        WebElement srcElement = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_1"));
+        WebElement destElement = driver.findElement(AppiumBy.id("io.appium.android.apis:id/drag_dot_2"));
+        dragNDropGesture(srcElement, destElement);
+    }
+}
